0a70f1a34d9aef5cb33a29ee22cf7a09c48f6ad4
lucasroth-github <lucas.roth@free.fr>
2021-06-02 19:04:45 +0200

    exercice py fini

--- Calcul-Pi-Arc-Tg-OK.py
+++ Calcul-Pi-Arc-Tg-OK.py
@@ -6,27 +6,26 @@ import random, time
 start_time = time.time()
 from math import *
  
-def arc_tangente(d,f,k,sem):
+def arc_tangente(d,f,k,sem,N):
     py = 0
     print(d,f)
     for i in range(d,f):
-        py += 4/(1+ ((i+0.5)/f)**2)
+        py += 4/(1+ ((i+0.5)/N)**2)
     sem.acquire()
-    k.value += (1/f)*py
+    k.value += py/N
     sem.release()
     
 
 if __name__ == "__main__" :
-    k=mp.Value('d',0)
+    k=mp.Value("d",0.0)
     sem=mp.Semaphore(1)
     N= 10**6
     Nb_process = 4
     mes_process = [0 for i in range(Nb_process)]
     for i in range(Nb_process):  # Lancer     Nb_process  processus
-            mes_process[i] = Process(target=arc_tangente, args= (int((i*N/4)),floor(((i+1)*N-1)/4),k,sem))
+            mes_process[i] = Process(target=arc_tangente, args= (int(i*N/Nb_process),floor(((i+1)*N -1)/ Nb_process),k,sem,N))
             mes_process[i].start()
-    
-    for i in range(Nb_process): mes_process[i].join()
+            mes_process[i].join()
     
     print("Valeur estimée Pi par la méthode Tangente : ", k.value)
     print("Temps d'execution : ", time.time() - start_time)
